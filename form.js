e79bb1718ede50a137882bddef51f50634b043ff
Muhammad Usman Jamil <jamilmuh@corp.contoursoftware.net>
2023-02-28 13:10:25 +0500

    added new fucntionalities

--- form.js
+++ form.js
@@ -1,4 +1,4 @@
-function myfun() {
+/*function myfun() {
   var username = document.querySelector("#username");
   ("\n");
   var email = document.querySelector("#email");
@@ -10,7 +10,7 @@ function myfun() {
   output.innerHTML += "Username: " + username.value;
   output.innerHTML += "User email: " + email.value;
   output.innerHTML += "User phone: " + phone.value;
-}
+}*/
 function GEEKFORGEEKS() {
   var name = document.forms.RegForm.username.value;
   var email = document.forms.RegForm.email.value;
@@ -75,4 +75,52 @@ function deletee(idd) {
   console.log(idd);
   let aa = document.getElementById(idd);
   aa.remove();
+}
+function contact(){
+  let a = document.getElementById("username").value;
+  let b = document.getElementById("email").value;
+  let c = document.getElementById("phone").value;
+  
+
+  var card = document.createElement("div");
+          card.classList.add("card");
+          card.innerHTML = `
+          <div class="ccard" style="background-color:whitesmoke;height:180px;width:400px ;margin-left:50px">
+                <h4 style="color:  #0D3AA9;padding-top: 10px;padding-left: 10px;">${a}</h4>
+                <div style="display: flex;"><img src="image/icon3.png">
+
+                    <p>${b}</p>
+
+                </div>
+                <div style="display: flex;"> <img src="image/icon4.png">
+                    <p>${c}</p>
+                </div>
+                <div style="display: flex; ;">
+                    <input type="submit" value="Edit" style="background-color:black;color:white;">
+                    <input type="submit" onclick="deleteCard()"  value="Delete" style="background-color:red;color:white;margin-left: 10px;">
+                </div>
+                <div>
+                    <div style="float: right;margin-top: -150px;">
+                        <input type="submit" value="Professional"
+                            style="background-color:green;color:white;display: flex;margin-bottom: 10px;">
+                        <img src="image/img3.png">
+                    </div>
+                </div>
+            </div>
+                `;
+                let NewCardd = document.getElementById("CARD");
+                NewCardd.appendChild(card);
+  
+}
+var addCardButton = document.getElementById("AddCard");
+addCardButton.addEventListener("click", contact);
+//   document.getElementById("myBtn").addEventListener("click", contact);
+function deleteCard() {
+// Find the first card element on the page
+const Cardd = document.querySelector('.ccard');
+
+// If a card element is found, remove it from the DOM
+if (Cardd) {
+Cardd.remove();
+}
 }
