14ff42ef81325e0f9fb9ce31dafc6fb904f43b06
UsmanJamil1998 <125854199+UsmanJamil1998@users.noreply.github.com>
2023-02-28 11:08:40 +0500

    it is Api work
    This work was taking time to upload

--- form.js
+++ form.js
@@ -1,50 +1,78 @@
 function myfun() {
-  var username = document.querySelector("#username"); "\n"
-  var email = document.querySelector("#email"); "\n"
-  var phone = document.querySelector("#phone"); "\n"
-  var output = document.querySelector("#output")
-  
+  var username = document.querySelector("#username");
+  ("\n");
+  var email = document.querySelector("#email");
+  ("\n");
+  var phone = document.querySelector("#phone");
+  ("\n");
+  var output = document.querySelector("#output");
+
   output.innerHTML += "Username: " + username.value;
   output.innerHTML += "User email: " + email.value;
   output.innerHTML += "User phone: " + phone.value;
-  
-
 }
 function GEEKFORGEEKS() {
-  var name =
-      document.forms.RegForm.username.value;
-  var email =
-      document.forms.RegForm.email.value;
-  var phone =
-      document.forms.RegForm.phone.value;
-  
-    
-  var regEmail=/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/g;  //Javascript reGex for Email Validation.
-  var regPhone=/^\d{11}$/;                                        // Javascript reGex for Phone Number validation.
-  var regName = /\d+$/g;                                    // Javascript reGex for Name validation
+  var name = document.forms.RegForm.username.value;
+  var email = document.forms.RegForm.email.value;
+  var phone = document.forms.RegForm.phone.value;
+
+  var regEmail = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/g; //Javascript reGex for Email Validation.
+  var regPhone = /^\d{11}$/; // Javascript reGex for Phone Number validation.
+  var regName = /\d+$/g; // Javascript reGex for Name validation
 
   if (name == "" || regName.test(name)) {
-      window.alert("Please enter your name properly.");
-      name.focus();
-      return false;
+    window.alert("Please enter your name properly.");
+    name.focus();
+    return false;
   }
 
-
-  
   if (email == "" || !regEmail.test(email)) {
-      window.alert("Please enter a valid e-mail address.");
-      email.focus();
-      return false;
-  }
-   
- 
-  
-  if (phone == "" || !regPhone.test(phone)) {
-      alert("Please enter valid phone number.");
-      phone.focus();
-      return false;
+    window.alert("Please enter a valid e-mail address.");
+    email.focus();
+    return false;
   }
 
-  
+  if (phone == "" || !regPhone.test(phone)) {
+    alert("Please enter valid phone number.");
+    phone.focus();
+    return false;
+  }
+
   return true;
+}
+fetch("https://jsonplaceholder.typicode.com/users")
+  .then((response) => response.json())
+  .then((json) => {
+    for (let i = 0; i < json.length; i++) {
+      console.log(json[i].id);
+      document.getElementById(
+        "CARD"
+      ).innerHTML += `<div style="background-color:whitesmoke;height:180px;width:400px ;margin-left:50px"id = "${json[i].id}">
+      <h4 style="color:  #0D3AA9;padding-top: 10px;padding-left: 10px;">${json[i].username}</h4>
+      <div style="display: flex;"><img src="image/icon3.png">
+
+          <p>${json[i].email}</p>
+
+      </div>
+      <div style="display: flex;"> <img src="image/icon4.png">
+          <p>${json[i].phone}</p>
+      </div>
+      <div style="display: flex; ;">
+          <input type="submit"onclick = "EditCard()" value="Edit" style="background-color:black;color:white;">
+          <input type="submit" onclick ="deletee(${json[i].id})" value="Delete" style="background-color:red;color:white;margin-left: 10px;">
+      </div>
+      <div>
+          <div style="float: right;margin-top: -150px;">
+              <input type="submit" value="Professional"
+                  style="background-color:green;color:white;display: flex;margin-bottom: 10px;">
+              <img src="image/img3.png">
+          </div>
+      </div>
+  </div>`;
+    }
+  });
+function deletee(idd) {
+  console.log(idd);
+  let aa = document.getElementById(idd);
+  aa.remove();
 }
